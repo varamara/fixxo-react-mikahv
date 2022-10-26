30ff96ff7cf3915fb7756230469d4e1192be0fc1
varamara <mikahvaarama@gmail.com>
2022-10-26 14:51:31 +0200

    Contact form validation added

--- src/sections/ContactFormSection.js
+++ src/sections/ContactFormSection.js
@@ -9,24 +9,45 @@ const ContactFormSection = () => {
     const validate = (values) => {
         const errors = {}
         const regex_email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-z]{2,}))$/
-  
+        const valuesName = values.name;
+        const valuesEmail = values.email;
+        const valuesComment = values.comment;
+
+
         // fixa regex till dessa med
         // // const regex_name =
         // // const regex_password =
 
-        if(!values.name)
+        
+
+        if(!valuesName) {
             errors.name = "You must enter a name"
+            document.getElementById('name').classList.add('error')
+        } else {
+            document.getElementById('name').classList.remove('error') 
+        }
 
-        if(!values.email)
-            errors.email = "You must enter an e-mail address"
-        else if (!regex_email.test(values.email))
-            errors.email = "You must enter a valid e-mail address (eg. name@domain.com)"
+        if(!valuesEmail) {
+        errors.email = "You must enter an e-mail address"
+        document.getElementById('email').classList.add('error')
+        }
+        else if (!regex_email.test(valuesEmail)) {
+        errors.email = "You must enter a valid e-mail address (eg. name@domain.com)"
+        document.getElementById('email').classList.add('error')
+        } else 
+        document.getElementById('email').classList.remove('error')
 
-        if(!values.comment)
-            errors.comment = "You must enter a comment"
-        else if(values.comment.length < 5)
-            errors.comment = "Your comment must be longer than 5 characters."
+        if(!valuesComment) {
+        errors.comment = "You must enter a comment"
+        document.getElementById('comment').classList.add('error')
+        }
+        else if(valuesComment.length < 5) {
+        errors.comment = "Your comment must be longer than 5 characters."
+        document.getElementById('comment').classList.add('error')
+        } else 
+        document.getElementById('comment').classList.remove('error')
 
+        
 
         if(Object.keys(errors).length === 0)
             setSubmitted(true)
@@ -47,41 +68,42 @@ const ContactFormSection = () => {
     }
 
     return (
-    <section className="contact-form">
-        <div className="container">
-          {
-            submitted ? (<div className="d-flex justify-content-center align-items-center">
-                <p>Thank you for commenting!</p>  
-                </div>)
+        <section className="contact-form">
+            <div className="container">
+            {
+                submitted ? (<div className="d-flex justify-content-center align-items-center">
+                    <p>Thank you for commenting!</p>  
+                    </div>)
 
-                :
-                (
-                    <>
-                        <h2>Come in Contact with Us</h2>
-                        <pre>{JSON.stringify(formErrors)}</pre>
-                        <form onSubmit={handleSubmit} noValidate>
-                            <div>
-                                <input id="name" type="text" placeholder="Your Name" value={contactForm.name} onChange={handleChange} />
-                                <div className="errorMessage">{formErrors.name}</div>
-                            </div>
-                            <div>
-                                <input id="email" type="email" placeholder="Your mail" value={contactForm.email} onChange={handleChange} />
-                                <div className="errorMessage">{formErrors.email}</div>
-                            </div>
-                            <div className="textarea">
-                                <textarea id="comment" placeholder="comments" value={contactForm.comment} onChange={handleChange} ></textarea>
-                                <div className="errorMessage">{formErrors.comment}</div>
-                            </div>
-                            <div>
-                                <button id="submit" type="submit" className="btn-theme"> Post Comments</button>
-                            </div>
-                        </form>
-                    </>
-                )
-            }
-        </div>
-    </section>
-  )
+                    :
+                    (
+                        <>
+                            <h2>Come in Contact with Us</h2>
+                            <form onSubmit={handleSubmit} noValidate>
+                                <div>
+                                    <input id="name" type="text" placeholder="Your Name" value={contactForm.name} onChange={handleChange} />
+                                    <div className="errorMessage">{formErrors.name}</div>
+                                </div>
+                                <div>
+                                    <input id="email" type="email" placeholder="Your mail" value={contactForm.email} onChange={handleChange} />
+                                    <div className="errorMessage">{formErrors.email}</div>
+                                </div>
+                                <div className="textarea">
+                                    <textarea id="comment" placeholder="comments" value={contactForm.comment} onChange={handleChange} ></textarea>
+                                    <div className="errorMessage">{formErrors.comment}</div>
+                                </div>
+                                <div>
+                                    <button id="submit" type="submit" className="btn-theme"> Post Comments</button>
+                                </div>
+                            </form>
+                        </>
+                    )
+                }
+            </div>
+        </section>
+    )
+ 
+
 }
 
 export default ContactFormSection
